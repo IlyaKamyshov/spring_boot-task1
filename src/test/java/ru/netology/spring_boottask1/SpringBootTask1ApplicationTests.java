fc25013b5dd0af819502633d690af26308dcbbbf
Ilya Kamyshov <kamyshov@gmail.com>
2024-05-28 13:30:03 +0300

    Initial commit for TestContainers

--- src/test/java/ru/netology/spring_boottask1/SpringBootTask1ApplicationTests.java
+++ src/test/java/ru/netology/spring_boottask1/SpringBootTask1ApplicationTests.java
@@ -1,13 +1,54 @@
 package ru.netology.spring_boottask1;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.junit.jupiter.Container;
 
-@SpringBootTest
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class SpringBootTask1ApplicationTests {
 
+    @Autowired
+    TestRestTemplate restTemplate;
+
+    @Container
+    private static final GenericContainer<?> devapp = new GenericContainer<>("devapp")
+            .withExposedPorts(8080);
+    @Container
+    private static final GenericContainer<?> prodapp = new GenericContainer<>("prodapp")
+            .withExposedPorts(8081);
+
+    @BeforeAll
+    public static void setUp() {
+        devapp.start();
+        prodapp.start();
+    }
     @Test
-    void contextLoads() {
+    void testDev() {
+        final String expected = "Current profile is dev";
+        int devAppPort = devapp.getMappedPort(8080);
+
+        ResponseEntity<String> devAppEntity = restTemplate.getForEntity(
+                "http://localhost:" + devAppPort + "/profile", String.class);
+
+        assertEquals(devAppEntity.getBody(), expected);
+    }
+
+    @Test
+    void testProd() {
+        final String expected = "Current profile is production";
+        int prodAppPort = prodapp.getMappedPort(8081);
+
+        ResponseEntity<String> prodAppEntity = restTemplate.getForEntity(
+                "http://localhost:" + prodAppPort + "/profile", String.class);
+
+        assertEquals(prodAppEntity.getBody(), expected);
     }
 
 }
